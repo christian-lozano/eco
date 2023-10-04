a8f42e0e9047c4f298e32c85a87502ad164f2aec
christian lozano <christian-lozano>
2023-10-04 12:00:58 -0500

    add filter desktop hover

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,11 +1,17 @@
 import { NavBottom } from './nav-bottom'
+import { NavHome } from './nav-home'
 import { NavTop } from './nav-top'
+
+/* ---------------------------------*/
+/* filtro menu normal a menu original*/
+/* ---------------------------------*/
 
 export function Nav() {
   return (
     <nav>
+      <NavHome />
       <NavTop />
-      <NavBottom />
+      <NavBottom /> 
     </nav>
   )
 }
