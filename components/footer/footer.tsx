d5bfa57625a0af4fb5528f811b7799291d5c2430
christian lozano <christian-lozano>
2023-10-05 09:49:01 -0500

    remove array footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -8,27 +8,7 @@ export type FooterProps = Record<string, unknown>
 export const Footer = memo(function Footer() {
   return (
     <footer className="mt-auto">
-      <div className=" bg-neutral-lightest justify-between px-20 py-24 laptop:flex">
-        {Array.from(Array(6), (ei, i) => (
-          <div key={i} className="flex flex-col gap-2">
-            <div className="label-semibold">Footer {i + 1}</div>
-            <ul className="small-regular flex flex-col gap-2">
-              {Array.from(Array(3), (ej, j) => (
-                <li key={j}>
-                  <Link
-                    href={`/footer-link-${i + 1}-${j + 1}`}
-                    title={`Link ${j + 1}`}
-                    className="text-neutral-darkest can-hover:transition-colors can-hover:hover:text-neutral-dark"
-                    onClick={(e) => e.preventDefault()}
-                  >
-                    {`Link ${j + 1}`}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </div>
+      <div className=" bg-neutral-lightest justify-between px-20 py-24 laptop:flex"></div>
 
       <div className="bg-brand-black flex flex-col items-center justify-center gap-3 py-8 laptop:py-9">
         <Link href="/" title="Spencer and Williams">
