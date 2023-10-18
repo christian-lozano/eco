79eebf7ea951609023c633c20cb459ec92910c35
christian lozano <christian-lozano>
2023-10-18 12:54:10 -0500

    add cart 6

--- pages/carrito/index.tsx
+++ pages/carrito/index.tsx
@@ -23,7 +23,7 @@ export default function Home(props: SearchPageLayoutProps) {
         <div className="mx-auto max-w-5xl 2xl:max-w-7xl justify-center px-1 md:flex md:space-x-6 xl:px-0">
           <div className="rounded-lg md:w-2/3">
             {domLoaded &&
-              items.map((el, i) => (
+              items.map((el) => (
                 <div
                   key={el.title}
                   className="justify-between mb-6 rounded-lg bg-white  shadow-md flex sm:justify-start"
@@ -69,9 +69,9 @@ export default function Home(props: SearchPageLayoutProps) {
                         />
                         <Button
                           className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"
-                            // onClick={() =>
-                            //   updateItemQuantity(el.id, el.quantity + 1)
-                            // }
+                          // onClick={() =>
+                          //   updateItemQuantity(el.id, el.quantity + 1)
+                          // }
                         >
                           {' '}
                           +{' '}
